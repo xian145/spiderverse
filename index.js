24003054a6c10711b5975de8adacb625ea4250f0
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 10:09:08 -0700

    se creo el archivo index donde se crea un objeto a partir de una clase

---
+++ index.js
@@ -0,0 +1,6 @@
+const spiderman = require('./app/spiderman');
+
+const Tom = new spiderman('Tom Holland', 25, "Peter Parker", 6, "Marvel")
+console.log(Tom);
+
+//se a verificado que el archivo haga lo que se pide
