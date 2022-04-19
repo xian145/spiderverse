31bf29d34b5e97ce64b59b6d6747b1a9624f333e
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 09:48:33 -0700

    se creo el archivo spiderman.js donde se encuntra la clase spiderman

---
+++ app/spiderman.js
@@ -0,0 +1,9 @@
+class spiderman {
+    constructor(name, age, actor, numberOfMovies, studio){
+        this.name = name,
+        this.age = age,
+        this.actor = actor,
+        this.numberOfMovies = numberOfMovies,
+        this.studio = studio
+    }
+}
