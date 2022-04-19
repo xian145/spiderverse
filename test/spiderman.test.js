84ea03a258b037372d8e9ddc8577527868dfec90
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 10:03:04 -0700

    se creo la prueba adecuada para la aplicacion sugerida

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,17 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+const spiderman = require('./../app/spiderman');
+
+describe("Unit test for spiderman class", () => {
+    test('1. Create an spiderman object', () => {
+        //instanciaremos un nuevo objeto con la informacion requerida, que es nombre, edad, actor, numero de peliculas y estudio de cine
+        const andrewG = new spiderman('Andrew Garfield', 38, 'Peter Parker', 3, 'Sony')
+
+        //comprobamos que el codigo funcione como queremos
+        expect(andrewG.name).toBe('Andrew Garfield'),
+        expect(andrewG.age).toBe(38),
+        expect(andrewG.actor).toBe('Peter Parker'),
+        expect(andrewG.numberOfMovies).toBe(3),
+        expect(andrewG.studio).toBe('Sony')
     });
 })
+
+//se a comprobado que la prueba funcione de manera correcta
